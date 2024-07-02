811afecde31a6688d197303755723f8846a64401
eduarda vasconcelos <eduardavasconcelos145@gmail.com>
2024-07-02 18:14:56 -0300

    classe livro

--- src/main/java/com/poo/classes/Livro.java
+++ src/main/java/com/poo/classes/Livro.java
@@ -4,7 +4,44 @@ public class Livro {
 int id;
 String edicao;
 String titulo;
-String ano;
-editora editora;
-autor autor;
+ int ano;
+ private editora editora;
+ private autor autor;
+
+public int getId() {
+    return id;
+}
+public void setId(int id) {
+    this.id = id;
+}
+public String getEdicao() {
+    return edicao;
+}
+public void setEdicao(String edicao) {
+    this.edicao = edicao;
+}
+public String getTitulo() {
+    return titulo;
+}
+public void setTitulo(String titulo) {
+    this.titulo = titulo;
+}
+public int getAno() {
+    return ano;
+}
+public void setAno(int ano) {
+    this.ano = ano;
+}
+public editora getEditora() {
+    return editora;
+}
+public void setEditora(editora editora) {
+    this.editora = editora;
+}
+public autor getAutor() {
+    return autor;
+}
+public void setAutor(autor autor) {
+    this.autor = autor;
+}
 }

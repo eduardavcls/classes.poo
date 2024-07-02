6195408eb370ed2fc342a8ef8cef40916d1a3998
eduarda vasconcelos <eduardavasconcelos145@gmail.com>
2024-07-02 18:14:20 -0300

    classe editora

--- src/main/java/com/poo/classes/editora.java
+++ src/main/java/com/poo/classes/editora.java
@@ -1,6 +1,26 @@
 package com.poo.classes;
 
 public class editora {
+public static String editora;
 int id;
 String nome;
+
+public static String getEditora() {
+    return editora;
+}
+public static void setEditora(String editora) {
+    editora.editora = editora;
+}
+public int getId() {
+    return id;
+}
+public void setId(int id) {
+    this.id = id;
+}
+public String getNome() {
+    return nome;
+}
+public void setNome(String nome) {
+    this.nome = nome;
+}
 }

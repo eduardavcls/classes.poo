829294054946985663822bc3c975103c5cdebdb4
eduarda vasconcelos <eduardavasconcelos145@gmail.com>
2024-07-02 18:12:33 -0300

    classe autor

--- src/main/java/com/poo/classes/autor.java
+++ src/main/java/com/poo/classes/autor.java
@@ -1,6 +1,21 @@
 package com.poo.classes;
 
 public class autor {
+public static String autor;
 int id;
 String nome;
+
+public int getId() {
+    return id;
+}
+public void setId() {
+    this.id = id;
+}
+public String getnome() {
+    return nome;
+}
+public void setnome(String nome) {
+    this.nome = nome;
+}
+
 }

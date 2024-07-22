41d9d3a6a6d31147a12413b28e1e0a9a84ba1578
eduarda vasconcelos <eduardavasconcelos145@gmail.com>
2024-07-22 17:44:21 -0300

    empréstimo

---
+++ src/main/java/com/poo/classes/exemplar/Emprestimo02.java
@@ -0,0 +1,36 @@
+import java.time.LocalDate;
+
+public class Emprestimo02 {
+private int id;
+private String pessoa;
+private int DataEmprestimo;
+private int DataPrevisaoDevolucao;
+
+public int getId() {
+    return id;
+}
+public void setId(int id) {
+    this.id = id;
+}
+public String getPessoa() {
+    return pessoa;
+}
+public void setPessoa(String pessoa) {
+    this.pessoa = pessoa;
+}
+public int getDataEmprestimo() {
+    return DataEmprestimo;
+}
+public void setDataEmprestimo(int dataEmprestimo) {
+    DataEmprestimo = dataEmprestimo;
+}
+public int getDataPrevisaoDevolucao() {
+    return DataPrevisaoDevolucao;
+}
+public void setDataPrevisaoDevolucao(int dataPrevisaoDevolucao) {
+    DataPrevisaoDevolucao = dataPrevisaoDevolucao;
+}
+
+
+    
+}

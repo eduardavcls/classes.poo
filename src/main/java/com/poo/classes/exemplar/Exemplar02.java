4ea5788b239f4751dac2feaf7173b949c63d3db8
eduarda vasconcelos <eduardavasconcelos145@gmail.com>
2024-07-22 17:40:03 -0300

    exemplar

---
+++ src/main/java/com/poo/classes/exemplar/Exemplar02.java
@@ -0,0 +1,19 @@
+public class Exemplar02 {
+private  int id;
+private String livro;
+
+public int getId() {
+    return id;
+}
+public void setId(int id) {
+    this.id = id;
+}
+public String getLivro() {
+    return livro;
+}
+public void setLivro(String livro) {
+    this.livro = livro;
+}
+ 
+    
+}

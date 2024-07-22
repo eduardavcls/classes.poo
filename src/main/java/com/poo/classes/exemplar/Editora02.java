b2ee389601344cace4a8a9897f36f94f43669eb1
eduarda vasconcelos <eduardavasconcelos145@gmail.com>
2024-07-22 17:40:38 -0300

    editora

---
+++ src/main/java/com/poo/classes/exemplar/Editora02.java
@@ -0,0 +1,32 @@
+public class Editora02 {
+private String editora;
+private int id;
+private String nome;
+
+//construtor
+public Editora02(String nome) {
+    this.nome = nome;
+}
+
+//getters and setters
+public String getEditora() {
+    return editora;
+}
+public void setEditora(String editora) {
+    this.editora = editora;
+}
+public int getId() {
+    return id;
+}
+public void setId(int id) {
+    this.id = id;
+}
+public String getNome() {
+    return nome;
+}
+public void setNome(String nome) {
+    this.nome = nome;
+}
+
+
+}

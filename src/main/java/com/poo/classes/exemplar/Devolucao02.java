c32faff67853c22245aad184acf2eec2c8aff857
eduarda vasconcelos <eduardavasconcelos145@gmail.com>
2024-07-22 17:44:57 -0300

    devolução

---
+++ src/main/java/com/poo/classes/exemplar/Devolucao02.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+
+public class Devolucao02 {
+private int id;
+private int DataDevolvido;
+
+
+public int getId() {
+    return id;
+}
+public void setId(int id) {
+    this.id = id;
+}
+public int getDataDevolvido() {
+    return DataDevolvido;
+}
+public void setDataDevolvido(int dataDevolvido) {
+    DataDevolvido = dataDevolvido;
+}
+
+ 
+    
+}
